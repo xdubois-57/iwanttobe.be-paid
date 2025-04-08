f652505b0c4fdc95223a552a23fdb701fffd9958
Xavier Dubois <xdubois@gmail.com>
2025-04-08 09:04:36 +0200

    Consolidate GDPR translations into a single file

--- translations/en.php
+++ translations/en.php
@@ -5,10 +5,5 @@ return array_merge(
     require __DIR__.'/en/errors.php',
     require __DIR__.'/en/home.php',
     require __DIR__.'/en/about.php',
-    require __DIR__.'/en/gdpr/general.php',
-    require __DIR__.'/en/gdpr/data_collection.php',
-    require __DIR__.'/en/gdpr/data_usage.php',
-    require __DIR__.'/en/gdpr/security.php',
-    require __DIR__.'/en/gdpr/rights.php',
-    require __DIR__.'/en/why_us.php'
+    require __DIR__.'/en/gdpr.php'
 );
