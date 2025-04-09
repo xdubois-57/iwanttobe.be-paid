8eead43abf576fa7ae8c5ae1f6faa8541784869f
Xavier Dubois <xdubois@gmail.com>
2025-04-09 08:49:18 +0200

    chore: add GNU GPLv3 license headers to JavaScript files
    - Add license headers to all JavaScript files
    - Place headers at the top of each file
    - Maintain proper JavaScript syntax and formatting
    - Skip configuration and deployment files

--- js/modules/translations.js
+++ js/modules/translations.js
@@ -1,4 +1,22 @@
 /**
+ * QR Transfer
+ * Copyright (C) 2025 Xavier Dubois
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Get a translation for a given key
  * @param {string} key - The translation key
  * @returns {string} - The translated text or the key if translation not found
