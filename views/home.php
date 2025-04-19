5c8b9de0cd50d660b68494287474a8938909879a
Xavier Dubois <xdubois@gmail.com>
2025-04-19 11:16:31 +0200

    fix: support link on home page now includes current language in URL

--- views/home.php
+++ views/home.php
@@ -107,13 +107,13 @@ require_once __DIR__ . '/header.php';
             <article class="qr-display">
                 <!-- Support QR code -->
                 <div id="support-qr" class="text-center">
-                <p class="support-message clickable" onclick="window.location.href='/support'">☕ <?php echo $lang->translate('support_text_alt'); ?></p>
+                <p class="support-message clickable" onclick="window.location.href='/' + <?php echo json_encode($lang->getCurrentLanguage()); ?> + '/support'">☕ <?php echo $lang->translate('support_text_alt'); ?></p>
                      
                 </div>
 
                 <!-- User generated QR -->
                 <div id="user-qr" class="text-center" style="display: none;">
-                    <p class="support-message clickable" onclick="window.location.href='/support'">☕ <?php echo $lang->translate('support_text_alt'); ?></p>
+                    <p class="support-message clickable" onclick="window.location.href='/' + <?php echo json_encode($lang->getCurrentLanguage()); ?> + '/support'">☕ <?php echo $lang->translate('support_text_alt'); ?></p>
                     <div class="qr-wrapper">
                         <img id="qr-image" src="" alt="Generated QR Code">
                     </div>
