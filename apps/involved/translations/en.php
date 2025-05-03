98e2b49f114c60a19fc1c9e8e70e48459364a270
Xavier Dubois <xdubois@gmail.com>
2025-05-03 14:12:35 +0200

    docs(translations): update en and fr translation files

--- apps/involved/translations/en.php
+++ apps/involved/translations/en.php
@@ -7,6 +7,7 @@ return array_merge(
     require __DIR__.'/en/home.php',
     require __DIR__.'/en/menu.php',
     [
-        'description' => 'Involved! makes it easy for teams, non-profits, and community groups to organize lively events, run engaging retrospectives, and launch interactive polls—turning any meeting into a memorable experience.'
+        'description' => 'Involved! makes it easy for teams, non-profits, and community groups to organize lively events, run engaging retrospectives, and launch interactive polls—turning any meeting into a memorable experience.',
+        'short_description' => 'Create interactive word clouds, polls, and retrospectives to engage your audience in real-time.',
     ]
 );
--- apps/involved/translations/fr.php
+++ apps/involved/translations/fr.php
@@ -7,6 +7,7 @@ return array_merge(
     require __DIR__.'/fr/home.php',
     require __DIR__.'/fr/menu.php',
     [
-        'description' => 'Involved! facilite pour les équipes, les organisations à but non lucratif et les groupes communautaires l\'organisation d\'événements animés, la tenue de rétrospectives engageantes et le lancement de sondages interactifs—transformant toute réunion en une expérience mémorable.'
+        'description' => 'Involved! facilite pour les équipes, les organisations à but non lucratif et les groupes communautaires l\'organisation d\'événements animés, la tenue de rétrospectives engageantes et le lancement de sondages interactifs—transformant toute réunion en une expérience mémorable.',
+        'short_description' => 'Créez des nuages de mots, des sondages et des rétrospectives interactifs pour impliquer votre audience en temps réel.',
     ]
 );
