8d77fffcfcb3b5111766a414658f150c8e9fc9d0
Xavier Dubois <xdubois@gmail.com>
2025-05-06 18:31:54 +0200

    feat: Add ≡ character to event items list for visual handle

--- apps/involved/views/event.php
+++ apps/involved/views/event.php
@@ -193,6 +193,7 @@ use chillerlan\QRCode\QROptions;
                     <li class="wordcloud-list-item">
                         <div class="wordcloud-list-content">
                             <span class="wordcloud-list-question">
+                                <span style="font-size:1.2em;vertical-align:middle;margin-right:0.5em;">≡</span>
                                 <?php echo htmlspecialchars($item['question']); ?>
                             </span>
                             <form method="post" action="/<?php echo htmlspecialchars($lang->getCurrentLanguage()); ?>/involved/<?php echo urlencode($eventData['key']); ?>/eventitem/<?php echo $item['id']; ?>/delete" style="display:inline;">
